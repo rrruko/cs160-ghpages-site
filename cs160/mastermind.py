82600bf1c75b50b83fc797bc0d3f481149f9594e
ruko <rukokarasu@gmail.com>
2017-10-27 21:02:17 -0700

    Add mastermind game and improve some layout stuff.

---
+++ cs160/mastermind.py
@@ -0,0 +1,40 @@
+import random
+
+RED = 'R'
+YELLOW = 'Y'
+GREEN = 'G'
+BLUE = 'B'
+COLORS = [RED, YELLOW, GREEN, BLUE]
+
+def main():
+    code = makeCode(COLORS, 4)
+    print(code)
+    for turnCount in range(0, 12):
+        guess = list(input('? '))
+        print(feedback(list(code), list(guess)))
+        if guess == code:
+            print('You win')
+            break
+
+
+def makeCode(colors, length):
+    return random.choices(colors, k=length)
+
+def feedback(code, guess):
+    out = ''
+    length = len(code)
+
+    for i in range(0, length):
+        if guess[i] == code[i]:
+            out += 'C'
+            code[i] = '_'
+
+    for i in range(0, length):
+        if guess[i] in code:
+            out += 'W'
+            code[code.index(guess[i])] = '_'
+
+    return out
+
+if __name__ == '__main__':
+    main()
