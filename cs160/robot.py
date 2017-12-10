31d69e2a3e2203250524240a1a663b5e0afaae47
ruko <rukokarasu@gmail.com>
2017-12-10 03:50:26 -0800

    Add collatz code

--- cs160/robot.py
+++ cs160/robot.py
@@ -1,6 +1,6 @@
 '''Robot treasure hunt'''
 
-import math
+from math import factorial, sqrt
 
 # pip install plotly and numpy
 import plotly
@@ -9,6 +9,7 @@ import numpy as np
 
 def distance_to_reach(energy, algo):
     '''Distance to reach energy at a given position using a given algorithm'''
+    if energy == 0: return 0
     i = 0
     dist = 0
     pos = 0
@@ -40,8 +41,16 @@ def factorial(n):
         return 1
     return n*factorial(n-1)
 
+def fib(n, computed = {0: 0, 1: 1}):
+    if n not in computed:
+        computed[n] = fib(n-1, computed) + fib(n-2, computed)
+    return computed[n]
+
+def fib2(n):
+    return (2*(n%2)-1) * fib(n)
+
 def fact(n):
-    return (2*(n%2)-1)*(factorial(n))
+    return (2*(n%2)-1) * factorial(n)
 
 def evaluate(algo):
     '''Print the performance of the algorithm at 50m, 100m, 500m, and 1000m'''
@@ -69,21 +78,27 @@ def make_graph():
     '''Write log plots of the algorithms to an HTML file'''
 
     # Plot the performance of the algorithm at intervals of 50 up to 10000
-    N = 10000
-    graph_x = list(range(1, N, 50))
+    N = 1000000
+    graph_x = list(range(0, N, N//200))
+    low_res = list(range(0, N, N//40))
 
     # Returns a named graph of the given algorithm
     def graph_algo(algo, name):
         y = list(map(lambda energy: avg_dist(energy, algo), graph_x))
         return go.Scatter(x=graph_x, y=y, name=name)
 
-    naive_graph = graph_algo(naive, 'linear')
+    naive_graph = go.Scatter(
+        x=low_res,
+        y=list(map(lambda energy: avg_dist(energy, naive), low_res)),
+        name='linear'
+    )
     square_graph = graph_algo(square, 'polynomial')
     pow2_graph = graph_algo(pow2, 'exponential')
+    fib_graph = graph_algo(fib2, 'fibonacci')
     fact_graph = graph_algo(fact, 'factorial')
 
     # We want to plot all the functions on one chart
-    data = [naive_graph, square_graph, pow2_graph, fact_graph]
+    data = [naive_graph, square_graph, pow2_graph, fib_graph, fact_graph]
 
     # The exponential and factorial algorithms are so much better that we need
     # to use a log plot to really see everything
